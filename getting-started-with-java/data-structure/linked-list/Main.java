34bf2a813695d5c2f6c629efbf016c60323fde34
Gustavo <gustavoleandrogorges@gmail.com>
2024-06-14 20:53:34 -0300

    testing linked list

--- getting-started-with-java/data-structure/linked-list/Main.java
+++ getting-started-with-java/data-structure/linked-list/Main.java
@@ -1,5 +1,25 @@
+import linkedlist.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello, Linked List!");
 
+        LinkedList<Integer> linkedList = new LinkedList<Integer>(10);
+        linkedList.append(20);
+        linkedList.append(30);
+        linkedList.append(40);
+
+        linkedList.printFromBeginning();
+
+        System.out.println("\nRemoving the last elements: ");
+
+        linkedList.pop();
+        linkedList.printFromBeginning();
+        System.out.println("\nRemoving one more element: ");
+        linkedList.pop();
+        linkedList.printFromBeginning();
+        System.out.println("\nRemoving one more element: ");
+        linkedList.pop();
+        linkedList.printFromBeginning();
     }
 }
