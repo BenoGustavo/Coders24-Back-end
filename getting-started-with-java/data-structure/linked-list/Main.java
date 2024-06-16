5a647b219161379b447fa3be58c3d74b7a372519
Gustavo <gustavoleandrogorges@gmail.com>
2024-06-15 21:14:58 -0300

    testing linked_list

--- getting-started-with-java/data-structure/linked-list/Main.java
+++ getting-started-with-java/data-structure/linked-list/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Linked List!");
 
-        LinkedList<Integer> linkedList = new LinkedList<Integer>(10);
+        LinkedList<Integer> linkedList = new LinkedList<>(10);
         linkedList.append(20);
         linkedList.append(30);
         linkedList.append(40);
 
-        System.out.println("\nPrinting from the start: ");
+        System.out.println(
+                "\nPrinting from the start, there is " + linkedList.getLength() + " elements in the linked list: ");
         linkedList.printFromBeginning();
 
         System.out.println("\nPrinting from the end: ");
@@ -20,11 +21,37 @@ public class Main {
 
         linkedList.pop();
         linkedList.printFromBeginning();
-        System.out.println("\nRemoving one more element: ");
         linkedList.pop();
+        System.out.println("\nRemoving one more element " + linkedList.getLength() + " elements in the linked list: ");
         linkedList.printFromBeginning();
-        System.out.println("\nRemoving one more element: ");
         linkedList.pop();
+        System.out.println("\nRemoving one more element " + linkedList.getLength() + " elements in the linked list:");
+        linkedList.printFromBeginning();
+
+        linkedList.makeEmpty();
+
+        System.out.println("\nLinked list is empty: " + linkedList.isEmpty() + " and has " + linkedList.getLength()
+                + " elements in the linked list: ");
+
+        linkedList.append(10);
+        linkedList.append(20);
+        linkedList.append(30);
+        linkedList.append(40);
+        linkedList.append(50);
+        linkedList.append(60);
+
+        System.out.println(
+                "\nPrinting from the start, there is " + linkedList.getLength() + " elements in the linked list: ");
+        linkedList.printFromBeginning();
+
+        System.out.println("\nRemoving the first element: ");
+        linkedList.deleteHead();
+        linkedList.printFromBeginning();
+
+        System.out.println("\nRemoving the element at index 3: ");
+
+        linkedList.deleteFromPosition(3);
+
         linkedList.printFromBeginning();
     }
 }
